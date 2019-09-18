a9e982f4686b667e20fa89fa1eb160559a943135
AddJun <1574118221@qq.com>
2019-09-19 00:49:12 +0800

    Transitiongroup

--- src/App.js
+++ src/App.js
@@ -1,41 +1,45 @@
 import React, { Component, Fragment } from 'react'
-import { CSSTransition } from 'react-transition-group';
+import { CSSTransition ,TransitionGroup} from 'react-transition-group';
 import './App.css'
-
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      show: true,
+      list: [],
     }
-    this.handleToggle = this.handleToggle.bind(this);
-    this.b = this.b.bind(this);
+    this.addList = this.addList.bind(this);
   }
   render() {
     return (
       <Fragment>
-        <CSSTransition
-          in={this.state.show}
-          classNames="hello"
-          timeout={3000}
-          //消失是空间消失
-          unmountOnExit
-          //触发函数的时间，实际上与css的过渡时间无关
-          onEntering={(el)=>{el.style.color = 'blue'}}
-          onEntered={()=>{this.a()}}
-          onExited={this.b}
-          appear={true}
-        >
-          <div>hello</div>
-        </CSSTransition>
-        <button onClick={this.handleToggle}>Toggle</button>
+        <TransitionGroup>
+          {
+            this.state.list.map((item,index)=>{
+              return (
+                <CSSTransition
+                  classNames="hello"
+                  timeout={1000}
+                  //消失是空间消失
+                  unmountOnExit
+                  //触发函数的时间，实际上与css的过渡时间无关
+                  onEntering={(el)=>{el.style.color = 'blue'}}
+                  appear={true}
+                  key={index}
+                >
+                  <div>{item}</div>
+                </CSSTransition>
+              )
+            })
+          }
+        </TransitionGroup>
+        <button onClick={this.addList}>Toggle</button>
       </Fragment>
     )
   }
-  handleToggle() {
+  addList() {
     this.setState((preState)=>({
-      show:preState.show ? false : true
+      list:[...preState.list,'item']
     }))
   }
   a(){
