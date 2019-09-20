f55b7ec0bd796b4fda90a1a7773afaaf7ef1d29a
AddJun <1574118221@qq.com>
2019-09-21 00:23:31 +0800

    react-redux使用Provider和connect还有两个映射函数，使得组件只需要往props拿数据就可以相当于取store的数据

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,41 +1,13 @@
-//引入actionTypes
-import { CHANGE_INPUT_VALUE, ADD_TODO_LIST, DELETE_TODO_ITEM, INIT_LIST } from './actionTypes'
-
-//初始化的state
 const defaultState = {
-  list: [],
-  inputValue: ''
-};
+  inputValue: '',
+  list: []
+}
 
-//reducer可以接受state，但不能修改state
-//reducer必须是纯函数，给固定的输入，有一定的固定输出，因此内部不能有ajax操作，而且不能有副作用
-const reducer = (state = defaultState, action) => {
-  // 改变输入值
-  if (action.type === CHANGE_INPUT_VALUE) {
+export default (state = defaultState, action) => {
+  if(action.type === 'change_input_value'){
     const newState = JSON.parse(JSON.stringify(state));
     newState.inputValue = action.value;
     return newState;
   }
-  // 添加列表项
-  if (action.type === ADD_TODO_LIST) {
-    const newState = JSON.parse(JSON.stringify(state));
-    newState.list.push(newState.inputValue);
-    newState.inputValue = '';
-    return newState;
-  }
-  //删除列表项
-  if (action.type === DELETE_TODO_ITEM) {
-    const newState = JSON.parse(JSON.stringify(state));
-    newState.list.splice(action.index, 1);
-    return newState;
-  }
-  //输出化列表
-  if (action.type === INIT_LIST) {
-    const newState = JSON.parse(JSON.stringify(state));
-    newState.list = action.list;
-    return newState;
-  }
   return state;
 }
-
-export default reducer;
--- src/todolist.js
+++ src/todolist.js
@@ -1,57 +1,37 @@
-import React, { Component } from 'react'
-import store from './store'
-import { doInitList,getInputChangeAction, getAddItemAction, getDeleteItemAction } from './store/actionCreator'
-import TodoListUI from './TodoListUI'
+import React, { Component, Fragment } from 'react'
+import { connect } from 'react-redux'
 
 class TodoList extends Component {
-  constructor(props) {
-    super(props);
-    //数据来源于store
-    this.state = store.getState();
-    //按钮点击
-    this.handleBtnClick = this.handleBtnClick.bind(this);
-    //输入内容变化
-    this.handleChange = this.handleChange.bind(this);
-
-    //订阅store的变化，当store变化则触发对应函数
-    this.handleStoreChange = this.handleStoreChange.bind(this)
-    store.subscribe(this.handleStoreChange);
-
-    this.handleItemClick = this.handleItemClick.bind(this);
-  }
   render() {
-    return <TodoListUI
-      inputValue={this.state.inputValue}
-      handleChange={this.handleChange}
-      handleBtnClick={this.handleBtnClick}
-      handleStoreChange={this.handleStoreChange}
-      handleItemClick={this.handleItemClick}
-      list={this.state.list}
-    />
-  }
-  componentDidMount() {
-    const action = doInitList();
-    store.dispatch(action)
-  }
-  // 改变input
-  handleChange(e) {
-    const action = getInputChangeAction(e.target.value);
-    store.dispatch(action);
-  }
-  //添加进list中
-  handleBtnClick() {
-    const action = getAddItemAction();
-    store.dispatch(action);
-  }
-  //store变化触发函数
-  handleStoreChange() {
-    //this.setState本来就是要接受一个参数，然后将对应需要更新的字段值进行替换
-    this.setState(store.getState());
-  }
-  //每一条点击消失
-  handleItemClick(index) {
-    const action = getDeleteItemAction(index);
-    store.dispatch(action);
+    return (
+      <Fragment>
+        <input type="text" value={this.props.inputValue} onChange={this.props.handleInputChange}/>
+        <button>提交</button>
+        <ul>
+          <li>hello</li>
+        </ul>
+      </Fragment>
+    )
   }
 }
-export default TodoList;
+
+//映射store到props
+const mapStateToProps = (state) => {
+  return {
+    inputValue: state.inputValue
+  }
+}
+//store.dispatch 挂载到props上
+const mapDispatchToProps = (dispatch) => {
+  return {
+    handleInputChange(e){
+      const action = {
+        type:'change_input_value',
+        value:e.target.value
+      }
+      dispatch(action);
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
